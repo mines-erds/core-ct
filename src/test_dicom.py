f26aab57fba2d341b90242edde634a803af360a0
TheSecondReal0 <66881186+TheSecondReal0@users.noreply.github.com>
2023-09-12 12:43:42 -0600

    commented test_dicom.py

--- src/test_dicom.py
+++ src/test_dicom.py
@@ -10,32 +10,38 @@ def main() -> None:
 
     args = parser.parse_args()
     dir: str = args.dir
-    
+
     dicom_from_dir(dir)
     glob_str = dir + "/*"
     dicom_from_files(glob(glob_str))
 
 def dicom_from_dir(dir: str) -> None:
     core = dicom(dir=dir)
-    test_core(core, "dicom_from_dir.png")
+    display_core(core, "dicom_from_dir.png")
 
 def dicom_from_files(files: list[str]) -> None:
     core = dicom(files=files)
-    test_core(core, "dicom_from_files.png")
+    display_core(core, "dicom_from_files.png")
 
-def test_core(core, output: str) -> None:
-    print(core.shape)
-    slice = core[:, :, 0]
-    print(slice.shape)
+def display_core(core, output: str, z: int = 0) -> None:
+    """
+    Output an image containing the slice at the provided z index in the dicom dataset
+    """
+    # retrieve slice data
+    slice = core[:, :, z]
+
+    # find the min and max brightness value to help with normalizing
     max: float = np.max(slice)
     min: float = np.min(slice)
-    print(max)
-    print(min)
+
+    # iterate through every datapoint in the slice and normalize it to be an integer between 0 and 255 (inclusive)
     offset = abs(min)
     norm_max = max + offset
     for i in range(len(slice)):
         for j in range(len(slice[0])):
             slice[i][j] = int(((slice[i][j] + offset) / norm_max) * 255)
+
+    # create an image depicting the slice using our normalized values
     picture = im.fromarray(slice)
     picture = picture.convert("L")
     picture.save(output)
