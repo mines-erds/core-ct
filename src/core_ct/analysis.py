35aa00b7a226543919f4065162a38efec42c0299
Atomator <30665220+Atomator@users.noreply.github.com>
2023-09-26 17:29:13 -0600

    Wrote testing functions for all three modules

--- src/core_ct/analysis.py
+++ src/core_ct/analysis.py
@@ -10,25 +10,10 @@ def brightness_trace(slice):
             standard deviation.
     """
 
-    # Initialize the result variables to store data in
-    brightness_result = np.zeros(0)
-    std_result = np.zeros(0)
-
-    # Step through each section of the slice that was passed in
-    for section in slice:
-        # Calculate the average brightness of that section and add it to the result array
-        brightness_of_slice = section.sum()
-        brightness_of_slice /= section.size
-        brightness_result = np.append(brightness_of_slice, brightness_result)
-
-        # Calculate the standard deviation of that slice and add it to the result array
-        std_of_slice = section.std()
-        std_result = np.append(std_of_slice, std_result)
     # Flip the two result arrays to make the orientation correct
-    brightness_result = np.flip(brightness_result)
-    std_result = np.flip(std_result)
+    brightness_result = np.mean(slice, axis=1)
+    std_result = np.std(slice, axis=1)
 
+    # TODO: Return this as a pandas dataframe with labels
     # Make the two result arrays into one returnable array
-    result = np.vstack((brightness_result, std_result))
-
-    return result
+    return np.array([brightness_result, std_result]).T
---
+++ tests/test_importers.py
@@ -0,0 +1,33 @@
+import os
+from core_ct import importers
+from glob import glob
+
+
+# Define the paths for the tests and scans directory
+tests_dir = os.path.dirname(os.path.realpath(__file__))
+scans_dir = os.path.join(tests_dir, 'scans')
+
+def test_import_from_files():
+    # Get the directory path for a specific scan
+    scan_dir = os.path.join(scans_dir, 'PAT_4636_39L_0001')
+    # Construct a list of all the files for the scan
+    scan_files = glob(scan_dir + '/PAT_4636_39L_*')
+
+    # Assert that there are ten files
+    assert len(scan_files) == 11
+
+    # Import the scan from the file list
+    core = importers.dicom(files=scan_files)
+
+    # Assert that the core imported correctly
+    assert core.pixel_array.shape == (512, 512, 11)
+
+def test_import_from_dir():
+    # Get the directory path for a specific scan
+    scan_dir = os.path.join(scans_dir, 'PAT_4636_39L_0001')
+
+    # Import the scan from the directory
+    core = importers.dicom(dir=scan_dir)
+
+    # Assert that the core imported correctly
+    assert core.pixel_array.shape == (512, 512, 11)
