627b1a7275b4ada586e1d9eba7d6bb235d8164fa
Atomator <30665220+Atomator@users.noreply.github.com>
2023-09-26 15:10:10 -0600

    Started writing pytest functions

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,18 +1,30 @@
-from core_ct import importers, analysis
-import matplotlib.pyplot as plt
-from os.path import dirname, realpath, join
+from core_ct import importers, analysis, Core
+import numpy as np
 
-# Get the test directory path
-tests_dir = dirname(realpath(__file__))
-# Get the core directory path
-core_dir = join(tests_dir, 'scans', 'PAT_4636_39L_0001')
+def test_importers():
+    importers.dicom
 
-# Import the core from the DICOM files
-core = importers.dicom(dir=core_dir)
+def test_analysis():
+    analysis.brightness_trace
 
-# Get a slice of the core and manually trim it
-slice = core.slice(axis=1, loc=10)
-slice = slice.T[:, 200:300]
+def test_core():
+    core = Core(np.zeros([2, 4, 8]), [1.0, 1.0, 1.0])
 
-# Create a brightness trace of the graph
-bt = analysis.brightness_trace(slice=slice)
+
+# import matplotlib.pyplot as plt
+# from os.path import dirname, realpath, join
+
+# # Get the test directory path
+# tests_dir = dirname(realpath(__file__))
+# # Get the core directory path
+# core_dir = join(tests_dir, 'scans', 'PAT_4636_39L_0001')
+
+# # Import the core from the DICOM files
+# core = importers.dicom(dir=core_dir)
+
+# # Get a slice of the core and manually trim it
+# slice = core.slice(axis=1, loc=10)
+# slice = slice.T[:, 200:300]
+
+# # Create a brightness trace of the graph
+# bt = analysis.brightness_trace(slice=slice)
