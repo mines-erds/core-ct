7c33446be396b0c4c12dae53656ab7604ae0cfeb
TheSecondReal0 <66881186+TheSecondReal0@users.noreply.github.com>
2023-10-18 20:03:13 -0600

    Adds tests for Core.flip function

--- tests/test_core.py
+++ tests/test_core.py
@@ -45,7 +45,8 @@ def test_swapaxes():
                 pixel_array[x, y, z] = counter
                 counter += 1
 
-    core: Core = Core(pixel_array=pixel_array, pixel_dimensions=[2.0, 4.0, 8.0])
+    core: Core = Core(pixel_array=copy.deepcopy(pixel_array), 
+                      pixel_dimensions=[2.0, 4.0, 8.0])
 
     # Swap various axes
     core_xy: Core = copy.deepcopy(core)
@@ -80,26 +81,96 @@ def test_swapaxes():
         # if this line is reached, swapaxes failed to raise an exception
         assert False
     except ValueError:
-        # nothing needs to be done if a ValueError was raised (expected behavior)
-        pass
+        # ValueError was raised (expected behavior), make sure no data was altered
+        assert np.array_equal(core.pixel_array, pixel_array)
+        # Make sure pixel_dimensions weren't altered
+        assert core.pixel_dimensions == [2.0, 4.0, 8.0]
     try:
         core.swapaxes(0, -1)
         # if this line is reached, swapaxes failed to raise an exception
         assert False
     except ValueError:
-        # nothing needs to be done if a ValueError was raised (expected behavior)
-        pass
+        # ValueError was raised (expected behavior), make sure no data was altered
+        assert np.array_equal(core.pixel_array, pixel_array)
+        # Make sure pixel_dimensions weren't altered
+        assert core.pixel_dimensions == [2.0, 4.0, 8.0]
     try:
         core.swapaxes(3, 0)
         # if this line is reached, swapaxes failed to raise an exception
         assert False
     except ValueError:
-        # nothing needs to be done if a ValueError was raised (expected behavior)
-        pass
+        # ValueError was raised (expected behavior), make sure no data was altered
+        assert np.array_equal(core.pixel_array, pixel_array)
+        # Make sure pixel_dimensions weren't altered
+        assert core.pixel_dimensions == [2.0, 4.0, 8.0]
     try:
         core.swapaxes(0, 3)
         # if this line is reached, swapaxes failed to raise an exception
         assert False
     except ValueError:
-        # nothing needs to be done if a ValueError was raised (expected behavior)
-        pass
+        # ValueError was raised (expected behavior), make sure no data was altered
+        assert np.array_equal(core.pixel_array, pixel_array)
+        # Make sure pixel_dimensions weren't altered
+        assert core.pixel_dimensions == [2.0, 4.0, 8.0]
+
+def test_flip():
+    """Tests the `flip` method on the `Core`."""
+    # Define the core
+    shape: list[int] = [2, 4, 8]
+    pixel_array: np.ndarray = np.zeros(shape)
+    counter: int = 0
+    for x in range(shape[0]):
+        for y in range(shape[1]):
+            for z in range(shape[2]):
+                pixel_array[x, y, z] = counter
+                counter += 1
+
+    core: Core = Core(pixel_array=copy.deepcopy(pixel_array), 
+                      pixel_dimensions=[2.0, 4.0, 8.0])
+
+    # Flip various axes
+    core_x: Core = copy.deepcopy(core)
+    core_x.flip(0)
+    core_y: Core = copy.deepcopy(core)
+    core_y.flip(1)
+    core_z: Core = copy.deepcopy(core)
+    core_z.flip(2)
+
+    # Check that the flip operation worked correctly on pixel_array
+    flip_x: np.ndarray = copy.deepcopy(pixel_array)
+    np.flip(flip_x, 0)
+    assert core_x.pixel_array.shape == flip_x.shape
+    assert np.array_equal(core_x.pixel_array, flip_x)
+    flip_y: np.ndarray = copy.deepcopy(pixel_array)
+    np.flip(flip_y, 1)
+    assert core_y.pixel_array.shape == flip_y.shape
+    assert np.array_equal(core_y.pixel_array, flip_y)
+    flip_z: np.ndarray = copy.deepcopy(pixel_array)
+    np.flip(flip_z, 2)
+    assert core_z.pixel_array.shape == flip_z.shape
+    assert np.array_equal(core_z.pixel_array, flip_z)
+
+    # Check that pixel_dimensions remained constant
+    assert core_x.pixel_dimensions == shape
+    assert core_y.pixel_dimensions == shape
+    assert core_z.pixel_dimensions == shape
+
+    # Check that Core.swapaxes() raises exceptions correctly
+    try:
+        core.flip(-1)
+        # if this line is reached, swapaxes failed to raise an exception
+        assert False
+    except ValueError:
+        # ValueError was raised (expected behavior), make sure no data was altered
+        assert np.array_equal(core.pixel_array, pixel_array)
+        # Make sure pixel_dimensions weren't altered
+        assert core.pixel_dimensions == [2.0, 4.0, 8.0]
+    try:
+        core.flip(3)
+        # if this line is reached, swapaxes failed to raise an exception
+        assert False
+    except ValueError:
+        # ValueError was raised (expected behavior), make sure no data was altered
+        assert np.array_equal(core.pixel_array, pixel_array)
+        # Make sure pixel_dimensions weren't altered
+        assert core.pixel_dimensions == [2.0, 4.0, 8.0]
